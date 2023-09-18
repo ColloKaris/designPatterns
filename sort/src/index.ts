326b4ed733d94b32da520200f0a82141c0cb4dd3
Collins Kariuki <collinsmunkary@gmail.com>
2023-09-18 08:12:02 +0300

    Working LinkedList

---
+++ sort/src/index.ts
@@ -0,0 +1,16 @@
+import { Sorter } from './Sorter.js';
+import { NumbersCollection } from './NumbersCollection.js';
+import { CharactersCollection } from './CharactersCollection.js';
+
+
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+// console.log('hello')
+
+const charactersCollection = new CharactersCollection('Xaayb');
+console.log(charactersCollection.data);
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
